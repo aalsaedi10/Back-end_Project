6eb64d21c3e05349962a0c622e6b7c56a7020f9c
aalsaedi10 <137519886+aalsaedi10@users.noreply.github.com>
2023-06-24 20:27:29 +0300

    first

---
+++ final_project/router/general.js
@@ -0,0 +1,68 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+
+
+public_users.post("/register", (req,res) => {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
+});
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+  if(books){
+    return res.json(books)
+  }
+  else{
+    return res.status(300).json({message: "there is no kooks"});
+  }
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
+ });
+  
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+  let author = req.params.author;
+  let book;
+  function json2array(json){
+    var result = [];
+    var keys = Object.keys(json);
+    keys.forEach(function(key){
+        result.push(json[key]);
+    });
+    return result;
+  }
+  let arr = json2array(books);
+  for(let i=0; i<arr.length;i++){
+    if(arr[i].author === author){
+      book = arr[i];
+      break;
+    }
+  }
+  if(book){
+    return res.json(book);
+  }
+  else{
+    return res.status(300).json({message: "not founded"});
+  }
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
+});
+
+module.exports.general = public_users;
